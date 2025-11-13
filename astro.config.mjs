601981374cbdaef2aa60eaee87a337b9e2eca370
Ednesor <edgar.funes2001@gmail.com>
2025-11-13 13:05:38 -0300

    fix config to delpoy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
     envPrefix: ["PUBLIC_", "PRIVATE_"],
   },
+  output: 'server',
+  adapter: netlify(),
 });
